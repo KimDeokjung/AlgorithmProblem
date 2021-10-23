7ad3103ba70f8c987c852f01551171c796732543
KimDeokjung <92872712+KimDeokjung@users.noreply.github.com>
2021-10-24 02:59:32 +0900

    Add files via upload
    백준문제 업데이트 브론즈 5

---
+++ BaekJoon/Python/Num10170.py
@@ -0,0 +1,6 @@
+# https://www.acmicpc.net/problem/10170
+# 숏코딩 파이썬 1등 / 전체 언어 1등
+
+a='      W   L  T '+'-'*23
+print('NFC West',a,'Seattle        13  3  0 San Francisco  12  4  0 Arizona        10  6  0 St. Louis      7   9  0\n NFC North'+a,'Green Bay      8   7  1 Chicago        8   8  0 Detroit        7   9  0 Minnesota      5  10  1')
+
---
+++ BaekJoon/Python/Num10699.py
@@ -0,0 +1,3 @@
+## https://www.acmicpc.net/problem/10699
+
+print('2021-10-23')
