dec53c6f2625430469227013d633d77663f45ce5
KimDeokjung <92872712+KimDeokjung@users.noreply.github.com>
2021-12-31 23:09:21 +0900

    Add files via upload
    백준 업데이트 (클래스 2)

--- BaekJoon/Bronze2/Num2775.py
+++ BaekJoon/Bronze2/Num2775.py
@@ -6,3 +6,13 @@ result = list()
 for x in range(T):
     k = int(input())
     n = int(input())
+
+    houseNum = list(range(1, n+1))
+    for y in range(k):
+        checkRoomNum = 0
+        for z in range(n):
+            checkRoomNum += houseNum[z]
+            houseNum[z] = checkRoomNum
+    result.append(houseNum[-1])
+
+print(*result)
