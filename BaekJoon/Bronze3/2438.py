47342b7c860a20d26f1aeaf15111382f5bb86eeb
KimDeokjung <92872712+KimDeokjung@users.noreply.github.com>
2022-02-27 23:29:03 +0900

    Rename 2438.py to Num2438.py

--- BaekJoon/Bronze3/2438.py
+++
@@ -1,3 +0,0 @@
-#https://www.acmicpc.net/problem/2438
-
-for x in range(int(input())):print('*'*(x+1))
---
+++ BaekJoon/Bronze3/Num2438.py
@@ -0,0 +1,3 @@
+#https://www.acmicpc.net/problem/2438
+
+for x in range(int(input())):print('*'*(x+1))
