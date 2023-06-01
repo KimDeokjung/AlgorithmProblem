24318d3e8d7438f108a8218e988e7b7e4b68a739
KimDeokjung <ejrwnd123@gmail.com>
2023-06-01 23:01:54 +0900

    [Gold4] 1043

--- BaekJoon/Gold4/Num1043.py
+++ BaekJoon/Gold4/Num1043.py
@@ -1,1 +1,25 @@
-print("정답!")
+N, M = map(int, input().split())
+
+tmp = list(map(int, input().split()[1:]))
+notLie = set(tmp)
+
+partyList = list()
+for x in range(M): partyList.append(list(map(int, input().split()[1:])))
+
+flag = set()
+while True:
+    newLie = set()
+
+    for x in range(len(partyList)):
+        if x in flag: continue
+
+        for y in notLie:
+            if y in partyList[x]:
+                flag.add(x)
+                newLie = newLie.union(set(partyList[x]))
+                break
+
+    if len(newLie) == 0: break
+    else: notLie = notLie.union(newLie)
+
+print(M - len(flag))
