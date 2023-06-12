854a5b03bb4f8b4773210fe3d3e8c28123172832
KimDeokjung <ejrwnd123@gmail.com>
2023-06-12 21:38:13 +0900

    [Silver3] 15657

--- BaekJoon/Silver3/Num15657.py
+++ BaekJoon/Silver3/Num15657.py
@@ -1,10 +1,27 @@
-import itertools
-
 N, M = map(int, input().split())
 
 inputData = list(map(int, input().split()))
 inputData.sort()
+checkSum = [0 for x in range(M)]
 
 
-for x in itertools.product(inputData, repeat = M):
-    print(*x)
+def abc(depth, data, flag):
+    if flag + 1 > N: return
+
+    if depth == M - 1:
+        for x in range(flag, N):
+            data[depth] = inputData[x]
+            print(*data)
+        return
+
+    for x in range(flag, N):
+        data[depth] = inputData[x]
+        abc(depth + 1, data, x)
+
+
+if M == 1:
+    print(*inputData)
+else:
+    for x in range(N):
+        checkSum[0] = inputData[x]
+        abc(1, checkSum, x)
