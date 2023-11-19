c1d9ca6e4680769517231b4b17fb973158f82c21
KimDeokjung <ejrwnd123@gmail.com>
2023-11-19 17:54:30 +0900

    재체점 수정

--- BaekJoon/Gold3/Num7579.py
+++ BaekJoon/Gold3/Num7579.py
@@ -5,6 +5,19 @@ totalCoast = sum(coast)
 checkSum = [[0 for _ in range(totalCoast + 1)] for __ in range(N + 1)]
 result = 10001
 
+tmpCoast = 0
+for x in range(1, N + 1):
+    if coast[x] == 0:
+        tmpCoast += application[x]
+        application[x] = 0
+
+for x in range(N + 1):
+    for y in range(totalCoast + 1):
+        checkSum[x][y] = tmpCoast
+
+if tmpCoast >= M:
+    print(0)
+    exit()
 
 for x in range(1, N + 1):
     for y in range(1, totalCoast + 1):
